5a0578671ac347686937be2f5a9aa962765afdfd
Lucas Valente <lucasvalente95@gmail.com>
2020-10-23 12:35:36 -0300

    :art: Configurando o modo do servidor Tomcat

--- Projeto Python.py
+++ Projeto Python.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import datetime
 import time
+import subprocess
 
 ## dentro do diretorioc > publicacoes > {criar pasta (data atual ## 2020-10-21)}dentro dessa pasta > {criar pasta (old)}dentro dessa pasta > {criar pasta (new)}
 
@@ -55,7 +56,102 @@ shutil.copyfile(src=src + '/online.war', dst=dst + '/online.war')
 
 
 
+## Criar um "stop" no servidor Tomcat
+
+scriptName = 'tomact_init.py'
+tomcatBinDir = '/apps/apache-tomcat-7.0.63/bin'
+tomcatShutdownPeriod = 5
+commandToCheckTomcatProcess = "ps -ef | grep -v grep | grep " + tomcatBinDir + " | wc -l" 
+commandToFindTomcatProcessId = 'ps -ef | grep -v grep | grep ' + tomcatBinDir + ' | awk \'{print $2}\''
+
+def isProcessRunning():
+    pStatus = True
+    tProcess = subprocess.Popen(commandToCheckTomcatProcess, stdout=subprocess.PIPE, shell=True)
+    out, err = tProcess.communicate()
+    if int(out) < 1:
+        pStatus = False
+    return pStatus
+
+def usage():
+    print ("Uso: python " + scriptName + " start|stop|status|restart")
+    print ("ou")
+    print ("Uso: <path>/" + scriptName + " start|stop|status|restart")
+
+def start():
+    if isProcessRunning():
+        print ("O processo Tomcat já está em execução")
+    else:
+        print ("Iniciando o tomcat")
+        subprocess.Popen([tomcatBinDir + "/startup.sh"], stdout=subprocess.PIPE)
+
+def stop():
+    if isProcessRunning():
+        print ("Parando o tomcat")
+        subprocess.Popen([tomcatBinDir + "/shutdown.sh"], stdout=subprocess.PIPE)
+        time.sleep(tomcatShutdownPeriod)
+        if isProcessRunning():
+            tPid = subprocess.Popen([commandToFindTomcatProcessId], stdout=subprocess.PIPE, shell=True)
+            out, err = tPid.communicate()
+            subprocess.Popen(["kill -9 " + out], stdout=subprocess.PIPE, shell=True)
+            print ("Tomcat falhou em desligar " + out)
+    else:
+       print ("O processo Tomcat não está em execução") 
+
+def status():
+    if isProcessRunning():
+        tPid = subprocess.Popen([commandToFindTomcatProcessId], stdout=subprocess.PIPE, shell=True)
+        out, err = tPid.communicate()
+        print ("O processo Tomcat está sendo executado " + out)
+    else:
+       print ("O processo Tomcat não está em execução") 
+
+if len(sys.argv) != 2:
+    print ("Missing argument")
+    usage()
+    sys.exit(0)
+else:
+    action = sys.argv[1]
+
+if action == 'start':
+    start()
+elif action == 'stop':
+    stop()
+elif action == 'status':
+    status() 
+elif action == 'restart':
+    stop()
+    start()
+else:
+    print ("Invalid argument")
+    usage()
+
+
+
+
 
 
 
 ## fazer backup da pasta videos para dentro do disco Co video está dentro da pasta c > servidores > webapps > online > $videos
+
+
+
+
+## Em Construcao !
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
